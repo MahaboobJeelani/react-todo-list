406730da7da26cb6ccf074f8c1f405dcf225a03c
Shaik MAhaboob Jeelani Basha <149247472+MahaboobJeelani@users.noreply.github.com>
2024-05-24 14:13:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import Tabs from './Components/Tabs'
-import Todolist from './Components/Todolist'
-import Stopwatch from './component/Stopwatch'
-import Jokes from './Jokes/Jokes'
 import Todotaskfil from './Todotaskfil/Todotaskfil'
-import Todotask from './Todotaskfil/Todotask'
 
 const App = () => {
   return (
